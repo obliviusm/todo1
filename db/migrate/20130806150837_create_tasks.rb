b6579be9b6860610901d7093fef790b133b58ce6
obliviusm <obliviusm@gmail.com>
2013-08-06 18:14:05 +0300

    bin/rails g model Task

---
+++ db/migrate/20130806150837_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+    	t.string :name
+    	t.boolean :status
+    	t.datetime :deadline
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806145140) do
+ActiveRecord::Schema.define(version: 20130806150837) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tasks", force: true do |t|
+    t.string   "name"
+    t.boolean  "status"
+    t.datetime "deadline"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
