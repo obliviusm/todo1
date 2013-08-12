ebd2be22785cd66118e87d79e20c180898dd5b90
obliviusm <obliviusm@gmail.com>
2013-08-12 11:53:12 +0300

    js update edit task. problems with create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,21 @@
 class TasksController < ApplicationController
 
+	def create
+		@task = Task.new
+		@task.name = params[:name]
+
+		@project = Project.find(params[:project_id])
+
+		respond_to do |format|
+			format.html { redirect_to project_path(@project) }
+			format.js
+		end
+	end
+
+	def new
+		@task = Task.new
+	end
+
 	def destroy
 		@task = Task.find(params[:id])
 		@task.destroy
@@ -9,7 +25,27 @@ class TasksController < ApplicationController
 
 	def edit
 		@task = Task.find(params[:id])
+		@project = Project.find(params[:project_id])
 
-		#format.js
+		respond_to do |format|
+			format.html 
+			format.js
+		end
+	end
+
+	def update
+		@task = Task.find(params[:id])
+		@task.update(task_params)
+
+		@project = Project.find(params[:project_id])
+
+		respond_to do |format|
+			format.html { redirect_to project_path(@project) }
+			format.js
+		end
+	end
+
+	def task_params
+		params.require(:task).permit(:name, :status, :deadline, :project_id)
 	end
 end
