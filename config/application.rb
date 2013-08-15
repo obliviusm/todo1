63d9546ae74cb7889915e417a93052426479fedd
obliviusm <obliviusm@gmail.com>
2013-08-15 16:06:45 +0300

    fdf

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Todo1
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
