98b7af1f19841b70ff2d68322eebe902eb0f68e4
obliviusm <obliviusm@gmail.com>
2013-08-15 13:08:06 +0300

    precompile

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,22 @@ module Todo1
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
+
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
