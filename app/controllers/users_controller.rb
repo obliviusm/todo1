31faf8e33241709e0e3a6a25e6ec29227f74aee8
obliviusm <obliviusm@gmail.com>
2013-09-04 21:50:11 +0300

    the end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
       redirect_to root_path
     else
       render 'new'
